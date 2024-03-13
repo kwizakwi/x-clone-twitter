cb324d58336a47d25bb094f613c2f7ce038f2426
kwizakwi <esaiebahati01@gmail.com>
2024-03-13 12:02:48 +0300

    creation et imolementation du side bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import './App.css'
 import Header from './Components/Header'
 import TweetEditor from './Components/Tweet-Editor'
 import Tweets from './Components/Tweets'
+import SideBar from './Components/sidebar/sidebar'
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        
+      <SideBar/>
+
+      </div>
       <div className="timeline">
         <Header />
         <TweetEditor />
         <Tweets />
       </div>
-      <div></div>
+      <div>Trend</div>
     </>
   )
 }
